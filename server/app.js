d3047d7f974fb371e9e714bf3bb8fff2215fafae
lodha1503 <lodhashreyansh1603@gmail.com>
2024-01-20 07:48:55 +0530

    modified packages

--- server/app.js
+++ server/app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const dotenv = require('dotenv');
 const productRoutes = require('./Routes/Product');
 const userRoutes = require('./Routes/User');
-const PORT = process.env.PORT || 6001
+const PORT = process.env.PORT || 5000
 
 const app = express();
-
+dotenv.config();
 app.use("/api/users/", userRoutes)
 app.use(express.json());
 app.use("/api/products/", productRoutes)
@@ -14,5 +14,6 @@ app.use("/api/products/", productRoutes)
 
 
 app.listen(PORT, () => {
+ 
   console.log(`Server is running on http://localhost:${PORT}`);
 });
